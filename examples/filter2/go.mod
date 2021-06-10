b9f3f562eba78048611d074c3702c9416491e318
Vitaliy Vlasov <siphiuel@gmail.com>
2021-06-10 15:59:51 +0300

    waku2-filter (#23)
    * waku2-filter

---
+++ examples/filter2/go.mod
@@ -0,0 +1,12 @@
+module filter2
+
+go 1.15
+
+require (
+	github.com/ethereum/go-ethereum v1.9.5
+	github.com/ipfs/go-log v1.0.5
+	github.com/multiformats/go-multiaddr v0.3.1 // indirect
+	github.com/status-im/go-waku v0.0.0-20210525134543-f6ebb5b0f9ae
+)
+
+replace github.com/status-im/go-waku => ../..
---
+++ examples/filter2/main.go
@@ -0,0 +1,161 @@
+package main
+
+import (
+	"context"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/ethereum/go-ethereum/crypto"
+	logging "github.com/ipfs/go-log"
+	"github.com/status-im/go-waku/waku/v2/node"
+	"github.com/status-im/go-waku/waku/v2/protocol/filter"
+	"github.com/status-im/go-waku/waku/v2/protocol/pb"
+	"github.com/status-im/go-waku/waku/v2/protocol/relay"
+)
+
+var log = logging.Logger("filter2")
+
+var pubSubTopic = relay.DefaultWakuTopic
+
+const contentTopic = "test"
+
+func main() {
+	lvl, err := logging.LevelFromString("info")
+	if err != nil {
+		panic(err)
+	}
+	logging.SetAllLoggers(lvl)
+
+	hostAddr1, _ := net.ResolveTCPAddr("tcp", fmt.Sprint("0.0.0.0:60000"))
+	hostAddr2, _ := net.ResolveTCPAddr("tcp", fmt.Sprint("0.0.0.0:60002"))
+
+	key1, err := randomHex(32)
+	if err != nil {
+		log.Error("Could not generate random key")
+		return
+	}
+	prvKey1, err := crypto.HexToECDSA(key1)
+
+	key2, err := randomHex(32)
+	if err != nil {
+		log.Error("Could not generate random key")
+		return
+	}
+	prvKey2, err := crypto.HexToECDSA(key2)
+
+	ctx := context.Background()
+
+	fullNode, err := node.New(ctx,
+		node.WithPrivateKey(prvKey1),
+		node.WithHostAddress([]net.Addr{hostAddr1}),
+		node.WithWakuRelay(),
+		node.WithWakuFilter(),
+	)
+
+	lightNode, err := node.New(ctx,
+		node.WithPrivateKey(prvKey2),
+		node.WithHostAddress([]net.Addr{hostAddr2}),
+		node.WithWakuRelay(),
+		node.WithWakuFilter(),
+	)
+
+	_, err = lightNode.AddFilterPeer(fullNode.ListenAddresses()[0])
+	if err != nil {
+		log.Info("Error adding filter peer on light node ", err)
+	}
+	//
+	// Setup filter
+	//
+
+	// Send FilterRequest from light node to full node
+	filterRequest := pb.FilterRequest{
+		ContentFilters: []*pb.FilterRequest_ContentFilter{&pb.FilterRequest_ContentFilter{ContentTopic: contentTopic}},
+		Topic:          string(pubSubTopic),
+		Subscribe:      true,
+	}
+
+	filterChan := make(filter.ContentFilterChan)
+
+	go func() {
+		for msg := range filterChan {
+			log.Info("Light node received msg, ", string(msg.Payload))
+		}
+	}()
+	lightNode.SubscribeFilter(ctx, filterRequest, filterChan)
+
+	go writeLoop(ctx, fullNode)
+	go readLoop(fullNode)
+
+	// Wait for a SIGINT or SIGTERM signal
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	<-ch
+	fmt.Println("\n\n\nReceived signal, shutting down...")
+
+	// shut the nodes down
+	fullNode.Stop()
+	lightNode.Stop()
+
+}
+
+func randomHex(n int) (string, error) {
+	bytes := make([]byte, n)
+	if _, err := rand.Read(bytes); err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(bytes), nil
+}
+
+func write(ctx context.Context, wakuNode *node.WakuNode, msgContent string) {
+	var version uint32 = 0
+	var timestamp float64 = float64(time.Now().UnixNano())
+
+	p := new(node.Payload)
+	p.Data = []byte(wakuNode.ID() + ": " + msgContent)
+	p.Key = &node.KeyInfo{Kind: node.None}
+
+	payload, err := p.Encode(version)
+
+	msg := &pb.WakuMessage{
+		Payload:      payload,
+		Version:      version,
+		ContentTopic: contentTopic,
+		Timestamp:    timestamp,
+	}
+
+	_, err = wakuNode.Publish(ctx, msg, nil)
+	if err != nil {
+		log.Error("Error sending a message: ", err)
+	}
+}
+
+func writeLoop(ctx context.Context, wakuNode *node.WakuNode) {
+	for {
+		time.Sleep(2 * time.Second)
+		write(ctx, wakuNode, "Hello world!")
+	}
+}
+
+func readLoop(wakuNode *node.WakuNode) {
+	sub, err := wakuNode.Subscribe(&pubSubTopic)
+	if err != nil {
+		log.Error("Could not subscribe: ", err)
+		return
+	}
+
+	for value := range sub.C {
+		payload, err := node.DecodePayload(value.Message(), &node.KeyInfo{Kind: node.None})
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		log.Info("Received msg, ", string(payload.Data))
+	}
+}
