748bbaf9930ff340f5a7c6a8138dca848429f38f
Vitaliy Vlasov <siphiuel@gmail.com>
2021-08-10 17:23:49 +0300

    Use keepAlive for Connectedness events

--- examples/peer_events/main.go
+++ examples/peer_events/main.go
@@ -7,6 +7,9 @@ import (
 	"encoding/hex"
 	"fmt"
 	"net"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/ethereum/go-ethereum/crypto"
@@ -14,7 +17,8 @@ import (
 	"github.com/status-im/go-waku/waku/v2/node"
 	"github.com/status-im/go-waku/waku/v2/protocol/pb"
 	"github.com/status-im/go-waku/waku/v2/protocol/relay"
-	"github.com/status-im/go-waku/waku/v2/protocol/store"
+
+	//"github.com/status-im/go-waku/waku/v2/protocol/store"
 	"github.com/status-im/go-waku/waku/v2/utils"
 )
 
@@ -55,107 +59,114 @@ func main() {
 
 	ctx := context.Background()
 
-	connStatusChan := make(chan node.ConnStatus)
+	//connStatusChan := make(chan node.ConnStatus)
+	log.Info("### create relayNode1")
 	relayNode1, err := node.New(ctx,
 		node.WithPrivateKey(addrsAndKeys[0].key),
 		node.WithHostAddress([]net.Addr{addrsAndKeys[0].addr}),
 		node.WithWakuRelay(),
-		node.WithConnStatusChan(connStatusChan),
+		//node.WithConnStatusChan(connStatusChan),
 		node.WithWakuStore(true),
+		node.WithKeepAlive(time.Duration(2)*time.Second),
 	)
 
-	relayNode2, err := node.New(ctx,
-		node.WithPrivateKey(addrsAndKeys[1].key),
-		node.WithHostAddress([]net.Addr{addrsAndKeys[1].addr}),
-		node.WithWakuRelay(),
-	)
+	// relayNode2, err := node.New(ctx,
+	// 	node.WithPrivateKey(addrsAndKeys[1].key),
+	// 	node.WithHostAddress([]net.Addr{addrsAndKeys[1].addr}),
+	// 	node.WithWakuRelay(),
+	// )
 
-	relayNode2.DialPeer(relayNode1.ListenAddresses()[0])
+	log.Info("### before DialPeer")
+	//staticNode := "/ip4/8.210.222.231/tcp/30303/p2p/16Uiu2HAm4v86W3bmT1BiH6oSPzcsSr24iDQpSN5Qa992BCjjwgrD"
+	staticNode := "/ip4/188.166.135.145/tcp/30303/p2p/16Uiu2HAmL5okWopX7NqZWBUKVqW8iUxCEmd5GMHLVPwCgzYzQv3e"
+	relayNode1.DialPeer(staticNode)
+	//relayNode2.DialPeer(relayNode1.ListenAddresses()[0])
 
 	//go writeLoop(ctx, relayNode1)
 	//go readLoop(relayNode1)
 
 	//go readLoop(relayNode2)
 
-	printNodeConns := func(node *node.WakuNode) {
-		log.Info(node.Host().ID(), ": ", "peerCount: ", len(node.Host().Peerstore().Peers()))
-		log.Info("node peers: ")
-		for k, v := range node.GetPeerStats() {
-			log.Info(k, " ", v)
-		}
-		log.Info(node.Host().ID(), ": ", "isOnline/hasHistory ", node.IsOnline(), " ", node.HasHistory())
-		log.Info("end")
+	log.Info("### Peer dialled")
+	// printNodeConns := func(node *node.WakuNode) {
+	// 	log.Info(node.Host().ID(), ": ", "peerCount: ", len(node.Host().Peerstore().Peers()))
+	// 	log.Info("node peers: ")
+	// 	for k, v := range node.GetPeerStats() {
+	// 		log.Info(k, " ", v)
+	// 	}
+	// 	log.Info(node.Host().ID(), ": ", "isOnline/hasHistory ", node.IsOnline(), " ", node.HasHistory())
+	// 	log.Info("end")
 
-	}
+	// }
 
-	go func() {
-		for connStatus := range connStatusChan {
-			log.Info("Conn status update: ", connStatus)
-		}
-	}()
-	go func() {
-		ticker := time.NewTicker(time.Millisecond * 1000)
-		defer ticker.Stop()
-		for {
-			select {
-			case <-ticker.C:
-				printNodeConns(relayNode1)
-			}
-		}
-	}()
+	// go func() {
+	// 	for connStatus := range connStatusChan {
+	// 		log.Info("Conn status update: ", connStatus)
+	// 	}
+	// }()
+	// go func() {
+	// 	ticker := time.NewTicker(time.Millisecond * 1000)
+	// 	defer ticker.Stop()
+	// 	for {
+	// 		select {
+	// 		case <-ticker.C:
+	// 			printNodeConns(relayNode1)
+	// 		}
+	// 	}
+	// }()
 
-	time.Sleep(3 * time.Second)
-	log.Info("stop relayNode2")
-	//relayNode2.Stop()
+	// time.Sleep(3 * time.Second)
+	// log.Info("stop relayNode2")
+	// relayNode2.Host().Close()
 
-	time.Sleep(3 * time.Second)
+	// time.Sleep(3 * time.Second)
 
-	log.Info("start relayNode3")
+	//	log.Info("start relayNode3")
 
-	relayNode3, err := node.New(ctx,
-		node.WithPrivateKey(addrsAndKeys[2].key),
-		node.WithHostAddress([]net.Addr{addrsAndKeys[2].addr}),
-		node.WithWakuRelay(),
-	)
+	//	relayNode3, err := node.New(ctx,
+	//		node.WithPrivateKey(addrsAndKeys[2].key),
+	//		node.WithHostAddress([]net.Addr{addrsAndKeys[2].addr}),
+	//		node.WithWakuRelay(),
+	//	)
 
-	relayNode3.DialPeer(relayNode1.ListenAddresses()[0])
+	//	relayNode3.DialPeer(relayNode1.ListenAddresses()[0])
 
-	time.Sleep(3 * time.Second)
-	log.Info("stop relayNode3")
-	//relayNode3.Stop()
+	//	time.Sleep(3 * time.Second)
+	//	log.Info("stop relayNode3")
+	//	//relayNode3.Stop()
 
-	log.Info("start storeNode")
-	// Start a store node
-	storeNode, _ := node.New(ctx,
-		node.WithPrivateKey(addrsAndKeys[3].key),
-		node.WithHostAddress([]net.Addr{addrsAndKeys[3].addr}),
-		node.WithWakuRelay(),
-		node.WithWakuStore(true),
-	)
-	tCtx, _ := context.WithTimeout(ctx, 5*time.Second)
-	log.Info("#before AddStorePeer")
-	storeNodeId, err := relayNode1.AddStorePeer(storeNode.ListenAddresses()[0])
-	time.Sleep(3 * time.Second)
-	log.Info("#before Query")
-	_, err = relayNode1.Query(tCtx, []string{contentTopic}, 0, 0, store.WithPeer(*storeNodeId))
-	log.Info("storeNode.ListenAddresses(): ", storeNode.ListenAddresses(), storeNodeId)
-	if err != nil {
-		log.Info("### error adding store peer: ", err)
-	}
+	//	log.Info("start storeNode")
+	//	// Start a store node
+	//	storeNode, _ := node.New(ctx,
+	//		node.WithPrivateKey(addrsAndKeys[3].key),
+	//		node.WithHostAddress([]net.Addr{addrsAndKeys[3].addr}),
+	//		node.WithWakuRelay(),
+	//		node.WithWakuStore(true),
+	//	)
+	//	tCtx, _ := context.WithTimeout(ctx, 5*time.Second)
+	//	log.Info("#before AddStorePeer")
+	//	storeNodeId, err := relayNode1.AddStorePeer(storeNode.ListenAddresses()[0])
+	//	time.Sleep(3 * time.Second)
+	//	log.Info("#before Query")
+	//	_, err = relayNode1.Query(tCtx, []string{contentTopic}, 0, 0, store.WithPeer(*storeNodeId))
+	//	log.Info("storeNode.ListenAddresses(): ", storeNode.ListenAddresses(), storeNodeId)
+	//	if err != nil {
+	//		log.Info("### error adding store peer: ", err)
+	//	}
 
-	time.Sleep(3 * time.Second)
-	log.Info("stop storeNode")
-	storeNode.Stop()
+	//	time.Sleep(3 * time.Second)
+	//	log.Info("stop storeNode")
+	//	storeNode.Stop()
 
-	time.Sleep(3 * time.Second)
-	// // Wait for a SIGINT or SIGTERM signal
-	// ch := make(chan os.Signal, 1)
-	// signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
-	// <-ch
-	// fmt.Println("\n\n\nReceived signal, shutting down...")
+	//	time.Sleep(3 * time.Second)
+	//	// // Wait for a SIGINT or SIGTERM signal
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	<-ch
+	fmt.Println("\n\n\nReceived signal, shutting down...")
 
-	// shut the nodes down
-	relayNode1.Stop()
+	//	// shut the nodes down
+	//	relayNode1.Stop()
 
 }
 
