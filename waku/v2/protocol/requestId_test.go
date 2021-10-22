7da2c28353719ff6238ea756f750103c97f29b16
Anthony Laibe <anthony@laibe.cc>
2021-10-22 14:57:57 +0200

    test: add test for generating requestid

---
+++ waku/v2/protocol/requestId_test.go
@@ -0,0 +1,15 @@
+package protocol
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGenerateRequestId(t *testing.T) {
+	// Force 2 reseed to ensure this is working as expected
+	for i := 1; i < 20001; i++ {
+		bytes := GenerateRequestId()
+		require.Equal(t, 32, len(bytes))
+	}
+}
