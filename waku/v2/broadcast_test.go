00692a51b0392cf7bfdb75bbbdae9b2bf523c1c5
Richard Ramos <info@richardramos.me>
2022-02-21 19:00:33 -0400

    fix: race condition in unsubscribe

--- waku/v2/broadcast_test.go
+++ waku/v2/broadcast_test.go
@@ -37,6 +37,33 @@ func TestBroadcast(t *testing.T) {
 	wg.Wait()
 }
 
+func TestBroadcastWait(t *testing.T) {
+	wg := sync.WaitGroup{}
+
+	b := NewBroadcaster(100)
+	defer b.Close()
+
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+
+		cch := make(chan *protocol.Envelope)
+		<-b.WaitRegister(cch)
+
+		go func() {
+			defer wg.Done()
+
+			<-cch
+			<-b.WaitUnregister(cch)
+		}()
+
+	}
+
+	env := new(protocol.Envelope)
+	b.Submit(env)
+
+	wg.Wait()
+}
+
 func TestBroadcastCleanup(t *testing.T) {
 	b := NewBroadcaster(100)
 	b.Register(make(chan *protocol.Envelope))
