dd0799a91b75c35439d1c5fe25883e9623def9dc
Richard Ramos <info@richardramos.me>
2022-05-27 15:55:35 -0400

    chore: upgrade deps and go1.17

--- waku/v2/discv5/discover_test.go
+++ waku/v2/discv5/discover_test.go
@@ -24,7 +24,7 @@ func createHost(t *testing.T) (host.Host, int, *ecdsa.PrivateKey) {
 	privKey, err := gcrypto.GenerateKey()
 	require.NoError(t, err)
 
-	sPrivKey := libp2pcrypto.PrivKey((*libp2pcrypto.Secp256k1PrivateKey)(privKey))
+	sPrivKey := libp2pcrypto.PrivKey(utils.EcdsaPrivKeyToSecp256k1PrivKey(privKey))
 
 	port, err := tests.FindFreePort(t, "127.0.0.1", 3)
 	require.NoError(t, err)
