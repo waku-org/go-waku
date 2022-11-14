dc0e4477bef5d7d06df40a91920adb17bd7b41b5
Richard Ramos <info@richardramos.me>
2022-11-14 10:21:16 -0400

    feat: find by criteria

--- waku/v2/protocol/store/waku_store_protocol_test.go
+++ waku/v2/protocol/store/waku_store_protocol_test.go
@@ -125,3 +125,72 @@ func TestWakuStoreProtocolNext(t *testing.T) {
 	require.NoError(t, err)
 	require.Len(t, response.Messages, 0)
 }
+
+func TestWakuStoreProtocolFind(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	host1, err := libp2p.New(libp2p.DefaultTransports, libp2p.ListenAddrStrings("/ip4/0.0.0.0/tcp/0"))
+	require.NoError(t, err)
+
+	db := MemoryDB(t)
+
+	s1 := NewWakuStore(host1, nil, db, utils.Logger())
+	s1.Start(ctx)
+	defer s1.Stop()
+
+	topic1 := "1"
+	pubsubTopic1 := "topic1"
+
+	msg1 := tests.CreateWakuMessage(topic1, 1)
+	msg2 := tests.CreateWakuMessage(topic1, 2)
+	msg3 := tests.CreateWakuMessage(topic1, 3)
+	msg4 := tests.CreateWakuMessage(topic1, 4)
+	msg5 := tests.CreateWakuMessage(topic1, 5)
+	msg6 := tests.CreateWakuMessage(topic1, 6)
+	msg7 := tests.CreateWakuMessage("hello", 7)
+	msg8 := tests.CreateWakuMessage(topic1, 8)
+	msg9 := tests.CreateWakuMessage(topic1, 9)
+
+	s1.MsgC <- protocol.NewEnvelope(msg1, utils.GetUnixEpoch(), pubsubTopic1)
+	s1.MsgC <- protocol.NewEnvelope(msg2, utils.GetUnixEpoch(), pubsubTopic1)
+	s1.MsgC <- protocol.NewEnvelope(msg3, utils.GetUnixEpoch(), pubsubTopic1)
+	s1.MsgC <- protocol.NewEnvelope(msg4, utils.GetUnixEpoch(), pubsubTopic1)
+	s1.MsgC <- protocol.NewEnvelope(msg5, utils.GetUnixEpoch(), pubsubTopic1)
+	s1.MsgC <- protocol.NewEnvelope(msg6, utils.GetUnixEpoch(), pubsubTopic1)
+	s1.MsgC <- protocol.NewEnvelope(msg7, utils.GetUnixEpoch(), pubsubTopic1)
+	s1.MsgC <- protocol.NewEnvelope(msg8, utils.GetUnixEpoch(), pubsubTopic1)
+	s1.MsgC <- protocol.NewEnvelope(msg9, utils.GetUnixEpoch(), pubsubTopic1)
+
+	host2, err := libp2p.New(libp2p.DefaultTransports, libp2p.ListenAddrStrings("/ip4/0.0.0.0/tcp/0"))
+	require.NoError(t, err)
+
+	host2.Peerstore().AddAddr(host1.ID(), tests.GetHostAddress(host1), peerstore.PermanentAddrTTL)
+	err = host2.Peerstore().AddProtocols(host1.ID(), string(StoreID_v20beta4))
+	require.NoError(t, err)
+
+	s2 := NewWakuStore(host2, nil, db, utils.Logger())
+	s2.Start(ctx)
+	defer s2.Stop()
+
+	q := Query{
+		Topic: pubsubTopic1,
+	}
+
+	fn := func(msg *pb.WakuMessage) (bool, error) {
+		return msg.ContentTopic == "hello", nil
+	}
+
+	foundMsg, err := s2.Find(ctx, q, fn, WithAutomaticPeerSelection(), WithAutomaticRequestId(), WithPaging(true, 2))
+	require.NoError(t, err)
+	require.NotNil(t, foundMsg)
+	require.Equal(t, "hello", foundMsg.ContentTopic)
+
+	fn2 := func(msg *pb.WakuMessage) (bool, error) {
+		return msg.ContentTopic == "bye", nil
+	}
+
+	foundMsg, err = s2.Find(ctx, q, fn2, WithAutomaticPeerSelection(), WithAutomaticRequestId(), WithPaging(true, 2))
+	require.NoError(t, err)
+	require.Nil(t, foundMsg)
+}
