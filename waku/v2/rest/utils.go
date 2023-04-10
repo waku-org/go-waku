5de3d9f619a95ac186089f8c67e7ead43bb2d673
Richard Ramos <info@richardramos.me>
2023-04-10 16:39:49 -0400

    feat(rest): store

--- waku/v2/rest/utils.go
+++ waku/v2/rest/utils.go
@@ -25,3 +25,20 @@ func writeErrOrResponse(w http.ResponseWriter, err error, value interface{}) {
 		return
 	}
 }
+
+func writeResponse(w http.ResponseWriter, value interface{}, code int) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	jsonResponse, err := json.Marshal(value)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	_, err = w.Write(jsonResponse)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(code)
+}
