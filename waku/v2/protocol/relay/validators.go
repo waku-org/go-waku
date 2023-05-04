0191afbb07fb3dd7ca71e4d7769a6140ba2561ad
Richard Ramos <info@richardramos.me>
2023-05-04 11:56:56 -0400

    feat: validate message timestamp in signed topic validator

--- waku/v2/protocol/relay/validators.go
+++ waku/v2/protocol/relay/validators.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"crypto/ecdsa"
 	"crypto/elliptic"
+	"encoding/binary"
 	"encoding/hex"
+	"time"
 
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/crypto/secp256k1"
@@ -13,24 +15,55 @@ import (
 	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/waku-org/go-waku/waku/v2/hash"
 	"github.com/waku-org/go-waku/waku/v2/protocol/pb"
+	"github.com/waku-org/go-waku/waku/v2/timesource"
 	"go.uber.org/zap"
 	proto "google.golang.org/protobuf/proto"
 )
 
 // Application level message hash
 func MsgHash(pubSubTopic string, msg *pb.WakuMessage) []byte {
-	// TODO: Other fields?
-	return hash.SHA256([]byte(pubSubTopic), msg.Payload, []byte(msg.ContentTopic))
+	timestampBytes := make([]byte, 8)
+	binary.LittleEndian.PutUint64(timestampBytes, uint64(msg.Timestamp))
+
+	var ephemeralByte byte
+	if msg.Ephemeral {
+		ephemeralByte = 1
+	}
+
+	return hash.SHA256(
+		[]byte(pubSubTopic),
+		msg.Payload,
+		[]byte(msg.ContentTopic),
+		timestampBytes,
+		[]byte{ephemeralByte},
+	)
+}
+
+const MessageWindowDuration = time.Minute * 5
+
+func withinTimeWindow(t timesource.Timesource, msg *pb.WakuMessage) bool {
+	if msg.Timestamp == 0 {
+		return false
+	}
+
+	now := t.Now()
+	msgTime := time.Unix(0, msg.Timestamp)
+
+	return now.Sub(msgTime).Abs() <= MessageWindowDuration
 }
 
 type validatorFn = func(ctx context.Context, peerID peer.ID, message *pubsub.Message) bool
 
-func validatorFnBuilder(topic string, publicKey *ecdsa.PublicKey) validatorFn {
+func validatorFnBuilder(t timesource.Timesource, topic string, publicKey *ecdsa.PublicKey) validatorFn {
 	pubkBytes := crypto.FromECDSAPub(publicKey)
 	return func(ctx context.Context, peerID peer.ID, message *pubsub.Message) bool {
 		msg := new(pb.WakuMessage)
 		err := proto.Unmarshal(message.Data, msg)
 		if err != nil {
+			return false
+		}
+
+		if !withinTimeWindow(t, msg) {
 			return false
 		}
 
@@ -43,7 +76,7 @@ func validatorFnBuilder(topic string, publicKey *ecdsa.PublicKey) validatorFn {
 
 func (w *WakuRelay) AddSignedTopicValidator(topic string, publicKey *ecdsa.PublicKey) error {
 	w.log.Info("adding validator to signed topic", zap.String("topic", topic), zap.String("publicKey", hex.EncodeToString(elliptic.Marshal(publicKey.Curve, publicKey.X, publicKey.Y))))
-	err := w.pubsub.RegisterTopicValidator(topic, validatorFnBuilder(topic, publicKey))
+	err := w.pubsub.RegisterTopicValidator(topic, validatorFnBuilder(w.timesource, topic, publicKey))
 	if err != nil {
 		return err
 	}
