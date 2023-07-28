c320b38cbecd3b0a20ec7e19d3451a9ea6c5e489
Richard Ramos <info@richardramos.me>
2023-07-28 13:25:33 -0400

    feat(rln-relay): use atomic operations API

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
           ];
           doCheck = false;
           # FIXME: This needs to be manually changed when updating modules.
-          vendorSha256 = "sha256-cMjGBz4/sDRcE55cfPBTl1lFkvHfZHeM4bTBcs5lO2s=";
+          vendorSha256 = "sha256-8khocBcz0klKwhoYKoPOvMSwFwa8rFcFSRM9jL8A0BU=";
           # Fix for 'nix run' trying to execute 'go-waku'.
           meta = { mainProgram = "waku"; };
         };
--- waku/v2/protocol/rln/group_manager/dynamic/handler_test.go
+++ waku/v2/protocol/rln/group_manager/dynamic/handler_test.go
@@ -59,8 +59,8 @@ func TestHandler(t *testing.T) {
 	roots = gm.rootTracker.Roots()
 
 	require.Len(t, roots, 2)
-	require.Equal(t, roots[0], root0)
-	require.Equal(t, roots[1], [32]byte{253, 232, 31, 10, 168, 25, 42, 0, 28, 221, 146, 119, 34, 212, 121, 51, 82, 55, 113, 181, 236, 3, 11, 190, 194, 144, 125, 59, 46, 171, 90, 43})
+	require.Equal(t, root0, roots[0])
+	require.Equal(t, [32]byte{0x96, 0x65, 0xdb, 0xbc, 0x28, 0x6b, 0x1f, 0xc6, 0xab, 0x2d, 0x82, 0xe, 0x30, 0xe0, 0xc1, 0x92, 0x32, 0x73, 0xd2, 0xd6, 0xe1, 0x21, 0xef, 0xa1, 0xc8, 0xa6, 0xa6, 0xd, 0x9, 0x7c, 0x85, 0x19}, roots[1])
 
 	events = []*contracts.RLNMemberRegistered{
 		eventBuilder(1, false, 0xbbbb, 2),
@@ -75,9 +75,9 @@ func TestHandler(t *testing.T) {
 	// Root[1] should become [0]
 	roots = gm.rootTracker.Roots()
 	require.Len(t, roots, 5)
-	require.Equal(t, roots[0], [32]byte{253, 232, 31, 10, 168, 25, 42, 0, 28, 221, 146, 119, 34, 212, 121, 51, 82, 55, 113, 181, 236, 3, 11, 190, 194, 144, 125, 59, 46, 171, 90, 43})
+	require.Equal(t, [32]byte{0x96, 0x65, 0xdb, 0xbc, 0x28, 0x6b, 0x1f, 0xc6, 0xab, 0x2d, 0x82, 0xe, 0x30, 0xe0, 0xc1, 0x92, 0x32, 0x73, 0xd2, 0xd6, 0xe1, 0x21, 0xef, 0xa1, 0xc8, 0xa6, 0xa6, 0xd, 0x9, 0x7c, 0x85, 0x19}, roots[0])
 	require.Len(t, rootTracker.Buffer(), 1)
-	require.Equal(t, rootTracker.Buffer()[0], [32]byte{62, 31, 25, 34, 223, 182, 113, 211, 249, 18, 247, 234, 70, 30, 10, 136, 238, 132, 143, 221, 225, 43, 108, 24, 171, 26, 210, 197, 106, 231, 52, 33})
+	require.Equal(t, root0, rootTracker.Buffer()[0])
 
 	// We detect a fork
 	//
