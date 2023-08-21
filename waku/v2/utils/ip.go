eb70aec4ca25339ac9f63ae859bb9cc48cf7af16
Prem Chaitanya Prathi <chaitanyaprem@gmail.com>
2023-08-21 17:25:00 +0530

    fix: ipv6 validation issue

--- waku/v2/utils/ip.go
+++ waku/v2/utils/ip.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"net"
+	"strings"
 )
 
 // IsIPv4 validates if string is a valid IPV4 address
@@ -12,6 +13,6 @@ func IsIPv4(str string) bool {
 
 // IsIPv6 validates if string is a valid IPV6 address
 func IsIPv6(str string) bool {
-	ip := net.ParseIP(str).To16()
-	return ip != nil
+	ip := net.ParseIP(str)
+	return ip != nil && strings.Contains(str, ":")
 }
--- waku/v2/utils/ip_test.go
+++ waku/v2/utils/ip_test.go
@@ -15,4 +15,6 @@ func TestIPValidation(t *testing.T) {
 	require.Equal(t, IsIPv6("::1"), true)
 	require.Equal(t, IsIPv6("fe80::6c4a:6aff:fecf:8097"), true)
 	require.Equal(t, IsIPv6("2001:0db8:85a3:0000:0000:8a2e:0370:7334:3445"), false)
+	require.Equal(t, IsIPv6("127.0.0.1"), false)
+
 }
