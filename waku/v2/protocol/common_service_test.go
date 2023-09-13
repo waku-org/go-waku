1ed38a2bcfeddd3224800d14515c507213cf09e4
harsh-98 <harshjniitr@gmail.com>
2023-09-13 09:08:31 +0700

    test: commonService start/stop execution

---
+++ waku/v2/protocol/common_service_test.go
@@ -0,0 +1,28 @@
+package protocol
+
+import (
+	"context"
+	"sync"
+	"testing"
+)
+
+// check if start and stop on common service works in random order
+func TestCommonService(t *testing.T) {
+	s := NewCommonService()
+	wg := &sync.WaitGroup{}
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		if i%2 == 0 {
+			go func() {
+				wg.Done()
+				s.Start(context.TODO(), func() error { return nil })
+			}()
+		} else {
+			go func() {
+				wg.Done()
+				go s.Stop(func() {})
+			}()
+		}
+	}
+	wg.Wait()
+}
