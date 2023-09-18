5d32705811cd0fcc9e6310d629dc9dc369271660
Prem Chaitanya Prathi <chaitanyaprem@gmail.com>
2023-09-18 12:26:55 +0530

    chore: modify relay test to cover unsubscribe and few more relay functions

--- waku/v2/protocol/relay/waku_relay_test.go
+++ waku/v2/protocol/relay/waku_relay_test.go
@@ -36,26 +36,34 @@ func TestWakuRelay(t *testing.T) {
 	defer sub.Cancel()
 	require.NoError(t, err)
 
+	require.Equal(t, relay.IsSubscribed(testTopic), true)
+
 	topics := relay.Topics()
 	require.Equal(t, 1, len(topics))
 	require.Equal(t, testTopic, topics[0])
 
 	ctx, cancel := context.WithCancel(context.Background())
-
+	bytesToSend := []byte{1}
 	go func() {
 		defer cancel()
 
 		_, err := sub.Next(ctx)
 		require.NoError(t, err)
+
 	}()
 
 	msg := &pb.WakuMessage{
-		Payload:      []byte{1},
+		Payload:      bytesToSend,
 		Version:      0,
 		ContentTopic: "test",
 		Timestamp:    0,
 	}
 	_, err = relay.PublishToTopic(context.Background(), msg, testTopic)
+	require.NoError(t, err)
+
+	time.Sleep(2 * time.Second)
+
+	err = relay.Unsubscribe(ctx, testTopic)
 	require.NoError(t, err)
 
 	<-ctx.Done()
