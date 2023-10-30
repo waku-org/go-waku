b5c45c01562a07c78a660d555106d3be69b3ebcf
Roman <zajic@zajic.net>
2023-10-30 18:34:23 +0800

    test: extend test - unsubscribe with single contentTopic

--- waku/v2/protocol/filter/filter_unsubscribe_test.go
+++ waku/v2/protocol/filter/filter_unsubscribe_test.go
@@ -1,20 +1,34 @@
 package filter
 
 func (s *FilterTestSuite) TestUnsubscribeSingleContentTopic() {
+
+	var newContentTopic = "TopicB"
+
 	// Initial subscribe
 	s.subDetails = s.subscribe(s.testTopic, s.testContentTopic, s.fullNodeHost.ID())
+	s.subDetails = s.subscribe(s.testTopic, newContentTopic, s.fullNodeHost.ID())
 
-	// Message should be received
+	// Message is possible to receive for original contentTopic
 	s.waitForMsg(func() {
 		s.publishMsg(s.testTopic, s.testContentTopic, "test_msg")
 	}, s.subDetails[0].C, false)
 
-	_ = s.unsubscribe(s.testTopic, s.testContentTopic, s.fullNodeHost.ID())
-
-	// Message should not be received
+	// Message is possible to receive for new contentTopic
 	s.waitForMsg(func() {
-		s.publishMsg(s.testTopic, s.testContentTopic, "test_msg")
+		s.publishMsg(s.testTopic, newContentTopic, "test_msg")
+	}, s.subDetails[0].C, false)
+
+	_ = s.unsubscribe(s.testTopic, newContentTopic, s.fullNodeHost.ID())
+
+	// Message should not be received for new contentTopic as it was unsubscribed
+	s.waitForMsg(func() {
+		s.publishMsg(s.testTopic, newContentTopic, "test_msg")
 	}, s.subDetails[0].C, true)
+
+	// Message is still possible to receive for original contentTopic
+	s.waitForMsg(func() {
+		s.publishMsg(s.testTopic, s.testContentTopic, "test_msg2")
+	}, s.subDetails[0].C, false)
 
 	_, err := s.lightNode.UnsubscribeAll(s.ctx)
 	s.Require().NoError(err)
