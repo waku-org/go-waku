ab5e2077ed7b0608854edbaa96dfa91edef333cf
Prem Chaitanya Prathi <chaitanyaprem@gmail.com>
2023-11-13 17:06:51 +0530

    feat: add option to specify peerAddr for filter subscribe

--- waku/v2/protocol/filter/options_test.go
+++ waku/v2/protocol/filter/options_test.go
@@ -5,6 +5,7 @@ import (
 	"crypto/rand"
 	"testing"
 
+	"github.com/multiformats/go-multiaddr"
 	"github.com/stretchr/testify/require"
 	"github.com/waku-org/go-waku/tests"
 	"github.com/waku-org/go-waku/waku/v2/utils"
@@ -47,6 +48,28 @@ func TestFilterOption(t *testing.T) {
 	for _, opt := range options2 {
 		err := opt(params2)
 		require.NoError(t, err)
+	}
+
+	require.NotNil(t, params2.selectedPeer)
+	require.True(t, params2.unsubscribeAll)
+
+	// Mutually Exclusive options
+	maddr, err := multiaddr.NewMultiaddr("/ip4/127.0.0.1/tcp/12345/p2p/16Uiu2HAm8KUwGRruseAaEGD6xGg6XKrDo8Py5dwDoL9wUpCxawGy")
+	require.NoError(t, err)
+	options3 := []FilterSubscribeOption{
+		WithPeer("16Uiu2HAm8KUwGRruseAaEGD6xGg6XKrDo8Py5dwDoL9wUpCxawGy"),
+		WithPeerAddr(maddr),
+	}
+
+	params3 := new(FilterSubscribeParameters)
+
+	for idx, opt := range options3 {
+		err := opt(params3)
+		if idx == 0 {
+			require.NoError(t, err)
+		} else {
+			require.Error(t, err)
+		}
 	}
 
 	require.NotNil(t, params2.selectedPeer)
