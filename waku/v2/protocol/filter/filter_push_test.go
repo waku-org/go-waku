a43a72339a71ea8a60d44a97959d6a94fb57d53d
Roman <zajic@zajic.net>
2023-11-14 18:56:41 +0800

    test: validate UTF8 payloads

--- waku/v2/protocol/filter/filter_push_test.go
+++ waku/v2/protocol/filter/filter_push_test.go
@@ -6,9 +6,7 @@ import (
 	"time"
 )
 
-// Test valid payloads
-
-func (s *FilterTestSuite) TestValidaPayloadsASCII() {
+func (s *FilterTestSuite) TestValidPayloadsASCII() {
 
 	// Create test context
 	s.ctx, s.ctxCancel = context.WithTimeout(context.Background(), 20*time.Second)
@@ -18,6 +16,27 @@ func (s *FilterTestSuite) TestValidaPayloadsASCII() {
 
 	// Prepare basic data
 	messages := prepareData(100, false, false, true, tests.GenerateRandomASCIIString)
+
+	// All messages should be received
+	s.waitForMessages(func() {
+		s.publishMessages(messages)
+	}, s.subDetails, messages)
+
+	_, err := s.lightNode.UnsubscribeAll(s.ctx)
+	s.Require().NoError(err)
+
+}
+
+func (s *FilterTestSuite) TestValidPayloadsUTF8() {
+
+	// Create test context
+	s.ctx, s.ctxCancel = context.WithTimeout(context.Background(), 20*time.Second)
+
+	// Subscribe
+	s.subDetails = s.subscribe(s.testTopic, s.testContentTopic, s.fullNodeHost.ID())
+
+	// Prepare basic data
+	messages := prepareData(100, false, false, true, tests.GenerateRandomUTF8String)
 
 	// All messages should be received
 	s.waitForMessages(func() {
