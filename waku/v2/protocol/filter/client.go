05e01540d06fdc2d615e5f0656f6f04846f63f3d
kaichaosun <kaichaosuna@gmail.com>
2023-12-19 08:38:01 +0800

    chore: recover within goroutine in filter light

--- waku/v2/protocol/filter/client.go
+++ waku/v2/protocol/filter/client.go
@@ -113,6 +113,7 @@ func (wf *WakuFilterLightNode) Stop() {
 		wf.h.RemoveStreamHandler(FilterPushID_v20beta1)
 		if wf.subscriptions.Count() > 0 {
 			go func() {
+				defer func() { recover() }()
 				res, err := wf.unsubscribeAll(wf.Context())
 				if err != nil {
 					wf.log.Warn("unsubscribing from full nodes", zap.Error(err))
@@ -618,6 +619,7 @@ func (wf *WakuFilterLightNode) unsubscribeAll(ctx context.Context, opts ...Filte
 				if params.wg != nil {
 					params.wg.Done()
 				}
+				recover()
 			}()
 
 			paramsCopy := params.Copy()
