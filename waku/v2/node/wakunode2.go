7a4eb842a48b4ff7feeec844890561d1ed9fd065
Prem Chaitanya Prathi <chaitanyaprem@gmail.com>
2024-02-07 16:32:45 +0530

    chore: cleanup old connStatus code

--- waku/v2/node/wakunode2.go
+++ waku/v2/node/wakunode2.go
@@ -112,21 +112,15 @@ type WakuNode struct {
 
 	bcaster relay.Broadcaster
 
-	connectionNotif        ConnectionNotifier
-	protocolEventSub       event.Subscription
-	identificationEventSub event.Subscription
-	addressChangesSub      event.Subscription
-	enrChangeCh            chan struct{}
+	connectionNotif   ConnectionNotifier
+	addressChangesSub event.Subscription
+	enrChangeCh       chan struct{}
 
 	keepAliveMutex sync.Mutex
 	keepAliveFails map[peer.ID]int
 
 	cancel context.CancelFunc
 	wg     *sync.WaitGroup
-
-	// Channel passed to WakuNode constructor
-	// receiving connection status notifications
-	connStatusChan chan<- ConnStatus
 
 	storeFactory storeFactory
 
@@ -364,14 +358,6 @@ func (w *WakuNode) Start(ctx context.Context) error {
 
 	w.host = host
 
-	if w.protocolEventSub, err = host.EventBus().Subscribe(new(event.EvtPeerProtocolsUpdated)); err != nil {
-		return err
-	}
-
-	if w.identificationEventSub, err = host.EventBus().Subscribe(new(event.EvtPeerIdentificationCompleted)); err != nil {
-		return err
-	}
-
 	if w.addressChangesSub, err = host.EventBus().Subscribe(new(event.EvtLocalAddressesUpdated)); err != nil {
 		return err
 	}
@@ -519,8 +505,6 @@ func (w *WakuNode) Stop() {
 	w.bcaster.Stop()
 
 	defer w.connectionNotif.Close()
-	defer w.protocolEventSub.Close()
-	defer w.identificationEventSub.Close()
 	defer w.addressChangesSub.Close()
 
 	w.host.Network().StopNotify(w.connectionNotif)
