a52787551a959ad947561e91bdc62b0972416bc3
Roman <zajic@zajic.net>
2024-03-14 15:28:53 +0800

    test: discV5 with shard filter

--- waku/v2/discv5/discover_test.go
+++ waku/v2/discv5/discover_test.go
@@ -3,6 +3,7 @@ package discv5
 import (
 	"context"
 	wps "github.com/waku-org/go-waku/waku/v2/peerstore"
+	wakuproto "github.com/waku-org/go-waku/waku/v2/protocol"
 	"github.com/waku-org/go-waku/waku/v2/service"
 	"go.uber.org/zap"
 	"testing"
@@ -223,4 +224,107 @@ func TestDiscV5WithCapabilitiesFilter(t *testing.T) {
 	d3.Stop()
 	peerconn3.Clear()
 
+}
+
+func TestDiscV5WithShardFilter(t *testing.T) {
+
+	// Following topic syntax for shard /waku/2/rs/<cluster_id>/<shard_number>
+	pubSubTopic := "/waku/2/rs/10/1"
+
+	// H1
+	host1, _, prvKey1 := tests.CreateHost(t)
+	udpPort1, err := tests.FindFreeUDPPort(t, "127.0.0.1", 3)
+	require.NoError(t, err)
+	ip1, _ := tests.ExtractIP(host1.Addrs()[0])
+	l1, err := tests.NewLocalnode(prvKey1, ip1, udpPort1, wenr.NewWakuEnrBitfield(true, true, true, true), nil, utils.Logger())
+	require.NoError(t, err)
+	peerconn1 := NewTestPeerDiscoverer()
+	d1, err := NewDiscoveryV5(prvKey1, l1, peerconn1, prometheus.DefaultRegisterer, utils.Logger(), WithUDPPort(uint(udpPort1)))
+	require.NoError(t, err)
+	d1.SetHost(host1)
+
+	// Derive shard from the topic
+	rs1, err := wakuproto.TopicsToRelayShards(pubSubTopic)
+	require.NoError(t, err)
+
+	// Update node with shard info
+	err = wenr.Update(l1, wenr.WithWakuRelaySharding(rs1[0]))
+	require.NoError(t, err)
+
+	// H2
+	host2, _, prvKey2 := tests.CreateHost(t)
+	ip2, _ := tests.ExtractIP(host2.Addrs()[0])
+	udpPort2, err := tests.FindFreeUDPPort(t, "127.0.0.1", 3)
+	require.NoError(t, err)
+	l2, err := tests.NewLocalnode(prvKey2, ip2, udpPort2, wenr.NewWakuEnrBitfield(true, true, false, true), nil, utils.Logger())
+	require.NoError(t, err)
+	peerconn2 := NewTestPeerDiscoverer()
+	d2, err := NewDiscoveryV5(prvKey2, l2, peerconn2, prometheus.DefaultRegisterer, utils.Logger(), WithUDPPort(uint(udpPort2)), WithBootnodes([]*enode.Node{d1.localnode.Node()}))
+	require.NoError(t, err)
+	d2.SetHost(host2)
+
+	// Update second node with shard info used for first node as well
+	err = wenr.Update(l2, wenr.WithWakuRelaySharding(rs1[0]))
+	require.NoError(t, err)
+
+	// H3
+	host3, _, prvKey3 := tests.CreateHost(t)
+	ip3, _ := tests.ExtractIP(host3.Addrs()[0])
+	udpPort3, err := tests.FindFreeUDPPort(t, "127.0.0.1", 3)
+	require.NoError(t, err)
+	l3, err := tests.NewLocalnode(prvKey3, ip3, udpPort3, wenr.NewWakuEnrBitfield(true, true, false, true), nil, utils.Logger())
+	require.NoError(t, err)
+	peerconn3 := NewTestPeerDiscoverer()
+	d3, err := NewDiscoveryV5(prvKey3, l3, peerconn3, prometheus.DefaultRegisterer, utils.Logger(), WithUDPPort(uint(udpPort3)), WithBootnodes([]*enode.Node{d2.localnode.Node()}))
+	require.NoError(t, err)
+	d3.SetHost(host3)
+
+	defer d1.Stop()
+	defer d2.Stop()
+	defer d3.Stop()
+
+	err = d1.Start(context.Background())
+	require.NoError(t, err)
+
+	err = d2.Start(context.Background())
+	require.NoError(t, err)
+
+	err = d3.Start(context.Background())
+	require.NoError(t, err)
+
+	// Create iterator with desired shard info
+	iterator3, err := d3.PeerIterator(FilterShard(rs1[0].ClusterID, rs1[0].ShardIDs[0]))
+
+	require.NoError(t, err)
+	require.NotNil(t, iterator3)
+
+	time.Sleep(2 * time.Second)
+
+	// Check node were discovered by automatic discovery
+	require.True(t, peerconn3.HasPeer(host1.ID()) && peerconn3.HasPeer(host2.ID()))
+
+	// Request two nodes
+	peers, err := discoverFilterOnDemand(iterator3, 2)
+	require.NoError(t, err)
+	require.Equal(t, 2, len(peers))
+
+	// Create map for checking
+	allPeers := make(map[string]interface{})
+	allPeers[host1.ID().String()] = struct{}{}
+	allPeers[host2.ID().String()] = struct{}{}
+	allPeers[host3.ID().String()] = struct{}{}
+
+	// Check nodes1 and nodes2 were discovered and node3 wasn't
+	for _, peer := range peers {
+		if _, found := allPeers[peer.AddrInfo.ID.String()]; found {
+			delete(allPeers, peer.AddrInfo.ID.String())
+		}
+	}
+
+	require.Equal(t, 1, len(allPeers))
+	_, host3Remains := allPeers[host3.ID().String()]
+	require.True(t, host3Remains)
+
+	d3.Stop()
+	peerconn3.Clear()
 }
