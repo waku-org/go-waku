dcb52aff2994aa71dac317f43151172ef1e468c3
Roman <zajic@zajic.net>
2024-03-15 18:49:05 +0800

    test: add discV5 with custom predicate

--- waku/v2/discv5/discover_test.go
+++ waku/v2/discv5/discover_test.go
@@ -356,3 +356,72 @@ func TestRecordErrorIteratorFailure(t *testing.T) {
 	require.Equal(t, 7, int(failures.GetCounter().GetValue()))
 
 }
+
+func TestDiscV5WithCustomPredicate(t *testing.T) {
+
+	// H1
+	host1, _, prvKey1 := tests.CreateHost(t)
+	udpPort1, err := tests.FindFreeUDPPort(t, "127.0.0.1", 3)
+	require.NoError(t, err)
+	ip1, _ := tests.ExtractIP(host1.Addrs()[0])
+	l1, err := tests.NewLocalnode(prvKey1, ip1, udpPort1, wenr.NewWakuEnrBitfield(true, true, true, true), nil, utils.Logger())
+	require.NoError(t, err)
+	peerconn1 := NewTestPeerDiscoverer()
+	d1, err := NewDiscoveryV5(prvKey1, l1, peerconn1, prometheus.DefaultRegisterer, utils.Logger(), WithUDPPort(uint(udpPort1)))
+	require.NoError(t, err)
+	d1.SetHost(host1)
+
+	// H2
+	host2, _, prvKey2 := tests.CreateHost(t)
+	ip2, _ := tests.ExtractIP(host2.Addrs()[0])
+	udpPort2, err := tests.FindFreeUDPPort(t, "127.0.0.1", 3)
+	require.NoError(t, err)
+	l2, err := tests.NewLocalnode(prvKey2, ip2, udpPort2, wenr.NewWakuEnrBitfield(true, true, true, true), nil, utils.Logger())
+	require.NoError(t, err)
+	peerconn2 := NewTestPeerDiscoverer()
+	d2, err := NewDiscoveryV5(prvKey2, l2, peerconn2, prometheus.DefaultRegisterer, utils.Logger(), WithUDPPort(uint(udpPort2)), WithBootnodes([]*enode.Node{d1.localnode.Node()}))
+	require.NoError(t, err)
+	d2.SetHost(host2)
+
+	// H3
+	blockAllPredicate := func(node *enode.Node) bool {
+		return false
+	}
+
+	host3, _, prvKey3 := tests.CreateHost(t)
+	ip3, _ := tests.ExtractIP(host3.Addrs()[0])
+	udpPort3, err := tests.FindFreeUDPPort(t, "127.0.0.1", 3)
+	require.NoError(t, err)
+	l3, err := tests.NewLocalnode(prvKey3, ip3, udpPort3, wenr.NewWakuEnrBitfield(true, true, true, true), nil, utils.Logger())
+	require.NoError(t, err)
+	peerconn3 := NewTestPeerDiscoverer()
+	d3, err := NewDiscoveryV5(prvKey3, l3, peerconn3, prometheus.DefaultRegisterer, utils.Logger(),
+		WithPredicate(blockAllPredicate), WithUDPPort(uint(udpPort3)),
+		WithBootnodes([]*enode.Node{d2.localnode.Node()}))
+	require.NoError(t, err)
+	d3.SetHost(host3)
+
+	defer d1.Stop()
+	defer d2.Stop()
+	defer d3.Stop()
+
+	err = d1.Start(context.Background())
+	require.NoError(t, err)
+
+	err = d2.Start(context.Background())
+	require.NoError(t, err)
+
+	err = d3.Start(context.Background())
+	require.NoError(t, err)
+
+	time.Sleep(2 * time.Second)
+
+	// Check none nodes were discovered by node3 as it is prevented by blockAllPredicate
+	require.False(t, peerconn3.HasPeer(host1.ID()) || peerconn3.HasPeer(host2.ID()))
+
+	// Check node2 could still discover node1 - predicate works at node scope only
+	require.True(t, peerconn2.HasPeer(host1.ID()))
+
+	d3.Stop()
+	peerconn3.Clear()
+}
