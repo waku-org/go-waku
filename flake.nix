cd70fbc912509f756959a7eaf11ee92534256df5
Jakub Sokołowski <jakub@status.im>
2024-04-06 14:20:57 +0300

    chore(nix): refactor, fix library packages
    Bit of a cleanup to make it more readable and also fix building of libraries.
    
    Moving the actual build to `default.nix` makes `flake.nix` more readable.
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- flake.nix
+++ flake.nix
@@ -11,11 +11,11 @@
       ];
       forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
 
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+      pkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
 
       buildPackage = system: subPackages:
         let
-          pkgs = nixpkgsFor.${system};
+          pkgs = pkgsFor.${system};
           commit = builtins.substring 0 7 (self.rev or "dirty");
           version = builtins.readFile ./VERSION;
         in pkgs.buildGo121Module {
@@ -34,17 +34,37 @@
           meta = { mainProgram = "waku"; };
         };
     in rec {
-      packages = forAllSystems (system: {
-        node    = buildPackage system ["cmd/waku"];
-        library = buildPackage system ["library/c"];
+      packages = forAllSystems (system: let
+        pkgs = pkgsFor.${system};
+        os = pkgs.stdenv.hostPlatform.uname.system;
+        sttLibExtMap = { Windows = "lib"; Darwin = "a";     Linux = "a";  };
+        dynLibExtMap = { Windows = "dll"; Darwin = "dylib"; Linux = "so"; };
+        buildPackage = pkgs.callPackage ./default.nix;
+      in rec {
+        default = node;
+        node = buildPackage {
+          inherit self;
+          subPkgs = ["cmd/waku"];
+        };
+        static-library = buildPackage {
+          inherit self;
+          subPkgs = ["library/c"];
+          ldflags = ["-buildmode=c-archive"];
+          output = "libgowaku.${sttLibExtMap.${os}}";
+        };
+        # FIXME: Compilation fails with:
+        #   relocation R_X86_64_TPOFF32 against runtime.tlsg can not be
+        #   used when making a shared object; recompile with -fPIC
+        dynamic-library = buildPackage {
+          inherit self;
+          subPkgs = ["library/c"];
+          ldflags = ["-buildmode=c-shared"];
+          output = "libgowaku.${dynLibExtMap.${os}}";
+        };
       });
 
-      defaultPackage = forAllSystems (system:
-        buildPackage system ["cmd/waku"]
-      );
-
       devShells = forAllSystems (system: let
-        pkgs = nixpkgsFor.${system};
+        pkgs = pkgsFor.${system};
         inherit (pkgs) lib stdenv mkShell;
       in {
         default = mkShell {
