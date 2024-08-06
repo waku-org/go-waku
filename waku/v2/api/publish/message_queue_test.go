e239db9623a92332da42ed27da4d401ec88532e7
Richard Ramos <info@richardramos.me>
2024-08-06 14:19:03 -0400

    fix: keep channels open

--- waku/v2/api/publish/message_queue_test.go
+++ waku/v2/api/publish/message_queue_test.go
@@ -17,25 +17,25 @@ func TestFifoQueue(t *testing.T) {
 	queue := NewMessageQueue(10, false)
 	go queue.Start(ctx)
 
-	queue.Push(protocol.NewEnvelope(&pb.WakuMessage{}, 0, "A"))
-	queue.Push(protocol.NewEnvelope(&pb.WakuMessage{}, 0, "B"))
-	queue.Push(protocol.NewEnvelope(&pb.WakuMessage{}, 0, "C"))
+	queue.Push(ctx, protocol.NewEnvelope(&pb.WakuMessage{}, 0, "A"))
+	queue.Push(ctx, protocol.NewEnvelope(&pb.WakuMessage{}, 0, "B"))
+	queue.Push(ctx, protocol.NewEnvelope(&pb.WakuMessage{}, 0, "C"))
 
-	envelope, ok := <-queue.Pop()
+	envelope, ok := <-queue.Pop(ctx)
 	require.True(t, ok)
 	require.Equal(t, "A", envelope.PubsubTopic())
 
-	envelope, ok = <-queue.Pop()
+	envelope, ok = <-queue.Pop(ctx)
 	require.True(t, ok)
 	require.Equal(t, "B", envelope.PubsubTopic())
 
-	envelope, ok = <-queue.Pop()
+	envelope, ok = <-queue.Pop(ctx)
 	require.True(t, ok)
 	require.Equal(t, "C", envelope.PubsubTopic())
 
 	cancel()
 
-	_, ok = <-queue.Pop()
+	_, ok = <-queue.Pop(ctx)
 	require.False(t, ok)
 }
 
@@ -45,47 +45,47 @@ func TestPriorityQueue(t *testing.T) {
 	queue := NewMessageQueue(10, true)
 	go queue.Start(ctx)
 
-	queue.Push(protocol.NewEnvelope(&pb.WakuMessage{Timestamp: proto.Int64(0)}, 0, "A"), LowPriority)
-	queue.Push(protocol.NewEnvelope(&pb.WakuMessage{Timestamp: proto.Int64(1)}, 0, "B"), LowPriority)
-	queue.Push(protocol.NewEnvelope(&pb.WakuMessage{Timestamp: proto.Int64(2)}, 0, "C"), HighPriority)
-	queue.Push(protocol.NewEnvelope(&pb.WakuMessage{Timestamp: proto.Int64(3)}, 0, "D"), NormalPriority)
-	queue.Push(protocol.NewEnvelope(&pb.WakuMessage{Timestamp: proto.Int64(4)}, 0, "E"), HighPriority)
-	queue.Push(protocol.NewEnvelope(&pb.WakuMessage{Timestamp: proto.Int64(5)}, 0, "F"), LowPriority)
-	queue.Push(protocol.NewEnvelope(&pb.WakuMessage{Timestamp: proto.Int64(6)}, 0, "G"), NormalPriority)
+	queue.Push(ctx, protocol.NewEnvelope(&pb.WakuMessage{Timestamp: proto.Int64(0)}, 0, "A"), LowPriority)
+	queue.Push(ctx, protocol.NewEnvelope(&pb.WakuMessage{Timestamp: proto.Int64(1)}, 0, "B"), LowPriority)
+	queue.Push(ctx, protocol.NewEnvelope(&pb.WakuMessage{Timestamp: proto.Int64(2)}, 0, "C"), HighPriority)
+	queue.Push(ctx, protocol.NewEnvelope(&pb.WakuMessage{Timestamp: proto.Int64(3)}, 0, "D"), NormalPriority)
+	queue.Push(ctx, protocol.NewEnvelope(&pb.WakuMessage{Timestamp: proto.Int64(4)}, 0, "E"), HighPriority)
+	queue.Push(ctx, protocol.NewEnvelope(&pb.WakuMessage{Timestamp: proto.Int64(5)}, 0, "F"), LowPriority)
+	queue.Push(ctx, protocol.NewEnvelope(&pb.WakuMessage{Timestamp: proto.Int64(6)}, 0, "G"), NormalPriority)
 
 	time.Sleep(2 * time.Second)
 
-	envelope, ok := <-queue.Pop()
+	envelope, ok := <-queue.Pop(ctx)
 	require.True(t, ok)
 	require.Equal(t, "C", envelope.PubsubTopic())
 
-	envelope, ok = <-queue.Pop()
+	envelope, ok = <-queue.Pop(ctx)
 	require.True(t, ok)
 	require.Equal(t, "E", envelope.PubsubTopic())
 
-	envelope, ok = <-queue.Pop()
+	envelope, ok = <-queue.Pop(ctx)
 	require.True(t, ok)
 	require.Equal(t, "D", envelope.PubsubTopic())
 
-	envelope, ok = <-queue.Pop()
+	envelope, ok = <-queue.Pop(ctx)
 	require.True(t, ok)
 	require.Equal(t, "G", envelope.PubsubTopic())
 
-	envelope, ok = <-queue.Pop()
+	envelope, ok = <-queue.Pop(ctx)
 	require.True(t, ok)
 	require.Equal(t, "A", envelope.PubsubTopic())
 
-	envelope, ok = <-queue.Pop()
+	envelope, ok = <-queue.Pop(ctx)
 	require.True(t, ok)
 	require.Equal(t, "B", envelope.PubsubTopic())
 
-	envelope, ok = <-queue.Pop()
+	envelope, ok = <-queue.Pop(ctx)
 	require.True(t, ok)
 	require.Equal(t, "F", envelope.PubsubTopic())
 
 	cancel()
 
-	_, ok = <-queue.Pop()
+	_, ok = <-queue.Pop(ctx)
 	require.False(t, ok)
 
 }
