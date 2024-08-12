288b704b3c13b252e30d38b2cc67f53842a74c0d
Václav Pavlín <vaclav.pavlin@gmail.com>
2024-08-12 15:20:24 +0200

    fix: add new logger handling chat2 example

--- examples/chat2/main.go
+++ examples/chat2/main.go
@@ -6,6 +6,7 @@ import (
 	logging "github.com/ipfs/go-log/v2"
 	"github.com/urfave/cli/v2"
 	"github.com/waku-org/go-waku/waku/v2/utils"
+	"go.uber.org/zap/zapcore"
 )
 
 var options Options
@@ -14,13 +15,13 @@ func main() {
 	app := &cli.App{
 		Flags: getFlags(),
 		Action: func(c *cli.Context) error {
-			utils.InitLogger("console", "file:chat2.log", "chat2")
-
-			lvl, err := logging.LevelFromString(options.LogLevel)
+			lvl, err := zapcore.ParseLevel(options.LogLevel)
 			if err != nil {
 				return err
 			}
-			logging.SetAllLoggers(lvl)
+
+			logging.SetAllLoggers(logging.LogLevel(lvl))
+			utils.InitLogger("console", "file:chat2.log", "chat2", lvl)
 
 			execute(options)
 			return nil
