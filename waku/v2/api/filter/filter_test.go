821481fec446591fe6e453f0c0880bab43bcefc5
Prem Chaitanya Prathi <chaitanyaprem@gmail.com>
2024-09-21 06:47:19 +0530

    fix: filter batch duration opt was not propagated correctly (#1224)

--- waku/v2/api/filter/filter_test.go
+++ waku/v2/api/filter/filter_test.go
@@ -54,7 +54,8 @@ func (s *FilterApiTestSuite) TestSubscribe() {
 	s.Require().Equal(contentFilter.PubsubTopic, s.TestTopic)
 	ctx, cancel := context.WithCancel(context.Background())
 	s.Log.Info("About to perform API Subscribe()")
-	apiSub, err := Subscribe(ctx, s.LightNode, contentFilter, apiConfig, s.Log)
+	params := subscribeParameters{300 * time.Second, 1024}
+	apiSub, err := Subscribe(ctx, s.LightNode, contentFilter, apiConfig, s.Log, &params)
 	s.Require().NoError(err)
 	s.Require().Equal(apiSub.ContentFilter, contentFilter)
 	s.Log.Info("Subscribed")
