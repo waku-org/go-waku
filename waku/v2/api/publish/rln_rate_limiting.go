7ff0d974565c2f6e322c5fdd6d3e04edcfcdb48f
kaichaosun <kaichaosuna@gmail.com>
2025-03-14 12:36:02 +0800

    feat: send bucket update when rate limit applied

--- waku/v2/api/publish/rln_rate_limiting.go
+++ waku/v2/api/publish/rln_rate_limiting.go
@@ -11,7 +11,7 @@ import (
 
 var ErrRateLimited = errors.New("rate limit exceeded")
 
-const RlnLimiterCapacity = 100
+const RlnLimiterCapacity = 600
 const RlnLimiterRefillInterval = 10 * time.Minute
 
 // RlnRateLimiter is used to rate limit the outgoing messages,
@@ -22,15 +22,23 @@ type RlnRateLimiter struct {
 	tokens         int
 	refillInterval time.Duration
 	lastRefill     time.Time
+	updateCh       chan BucketUpdate
+}
+
+// BucketUpdate includes the information that need to be persisted in database.
+type BucketUpdate struct {
+	RemainingTokens int
+	LastRefill      time.Time
 }
 
 // NewRlnPublishRateLimiter creates a new rate limiter, starts with a full capacity bucket.
-func NewRlnRateLimiter(capacity int, refillInterval time.Duration) *RlnRateLimiter {
+func NewRlnRateLimiter(capacity int, refillInterval time.Duration, availableTokens int, lastRefill time.Time, updateCh chan BucketUpdate) *RlnRateLimiter {
 	return &RlnRateLimiter{
 		capacity:       capacity,
-		tokens:         capacity, // Start with a full bucket
+		tokens:         availableTokens, // Start with a full bucket in the first run, then track the remaining tokens in storage
 		refillInterval: refillInterval,
-		lastRefill:     time.Now(),
+		lastRefill:     lastRefill,
+		updateCh:       updateCh,
 	}
 }
 
@@ -42,17 +50,24 @@ func (rl *RlnRateLimiter) Allow() bool {
 	// Refill tokens if the refill interval has passed
 	now := time.Now()
 	if now.Sub(rl.lastRefill) >= rl.refillInterval {
-		rl.tokens = rl.capacity // Refill the bucket
+		rl.tokens = rl.capacity
 		rl.lastRefill = now
+		rl.sendUpdate()
 	}
 
 	// Check if there are tokens available
 	if rl.tokens > 0 {
 		rl.tokens--
+		rl.sendUpdate()
 		return true
 	}
 
 	return false
+}
+
+// sendUpdate sends the latest token state to the update channel.
+func (rl *RlnRateLimiter) sendUpdate() {
+	rl.updateCh <- BucketUpdate{RemainingTokens: rl.tokens, LastRefill: rl.lastRefill}
 }
 
 func (rl *RlnRateLimiter) Check(ctx context.Context, logger *zap.Logger) error {
