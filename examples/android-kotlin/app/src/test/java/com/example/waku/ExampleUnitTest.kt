1b7921bf55f4ddb3809c3d560f3461d9ce5d4c3b
John Water <fits_biofilm_0n@icloud.com>
2025-03-27 12:17:36 +0400

    fix-Update ExampleUnitTest.kt

--- examples/android-kotlin/app/src/test/java/com/example/waku/ExampleUnitTest.kt
+++ examples/android-kotlin/app/src/test/java/com/example/waku/ExampleUnitTest.kt
@@ -7,7 +7,7 @@ import org.junit.Assert.*
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
- * See [testing documentation](http://d.android.com/tools/testing).
+ * See [testing documentation](https://developer.android.com/tools/testing).
  */
 class ExampleUnitTest {
     @Test
